59fb458b514f9031bc1cc322488e9f41db8c5d07
Jonny <jonny.covert@gmail.com>
2013-01-20 09:05:47 -0500

    Changed .gitignore file

--- bot1.js
+++ bot1.js
@@ -15,13 +15,13 @@ function main(){
 		var ang = Math.atan2(a.y, a.x);
 		var vel = bot.vel;
 		var velMag = vel.mag();
-
-		if(-sign(vel.x) == sign(Math.cos(ang))){ //Both Kinetic force and applied force are acking in the same direction
+		
+		if(-sign(vel.x) == sign(Math.cos(ang))){ //Both Kinetic force and applied force are acting in the same direction
 			bot.force.x = Math.cos(ang)*velMag*velMag/a.mag()-sign(Math.cos(ang));
 		}else{
 			bot.force.x = Math.cos(ang)*velMag*velMag/a.mag()+sign(Math.cos(ang));
 		}
-		if(-sign(vel.y) == sign(Math.sin(ang))){ //Both Kinetic force and applied force are acking in the same direction
+		if(-sign(vel.y) == sign(Math.sin(ang))){ //Both Kinetic force and applied force are acting in the same direction
 			bot.force.y = Math.sin(ang)*velMag*velMag/a.mag()-sign(Math.sin(ang));
 		}else{
 			bot.force.y = Math.sin(ang)*velMag*velMag/a.mag()+sign(Math.sin(ang));
